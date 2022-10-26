ac2c21c6004b8b2c026e3c9c7ecfd7573e12fee5
Sublimez72 <vaggge@hotmail.com>
2022-10-26 19:21:58 +0200

    First part of chapter 1 code exercise

---
+++ Chapter1-1.py
@@ -0,0 +1,21 @@
+print("Hello world")
+
+import this
+
+name = "Vangelis"
+city = "Stockholm"
+age = 24
+
+print("Hello my name is {0} and I am {1} years old and live in {2}".format(name, age, city))
+print(f"Hello my name is {name} and I live in {city}, and I am {age} years old")
+
+pi = 3.14159
+
+print("%d" % (pi))
+print("%e" % (pi))
+print("%s" % (pi))
+
+import pdb
+pdb.set_trace()
+
+print("done")
