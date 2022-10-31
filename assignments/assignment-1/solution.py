c77b42744b5de920d7bf879966d86805f3f3511a
Sublimez72 <vaggge@hotmail.com>
2022-10-31 14:12:06 +0100

    Finished Assignment 1

--- assignments/assignment-1/solution.py
+++ assignments/assignment-1/solution.py
@@ -1,5 +1,18 @@
-def solution(s):
-    """
-    Expected return value is a str
-    """
-    return ""
+
+
+def solution(s=str):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    # Create the translation table
+    trans = str.maketrans(alphabet, alphabet[::-1] , " ")
+    result = ""
+    # Iterate through the string
+    for letter in s:
+        # if the letter is lowercase we translate it using our translation table
+        if letter.islower():
+            result += letter.translate(trans)
+        else:
+            result += letter
+    
+    
+    return result
+
