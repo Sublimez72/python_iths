0d91fae8b5e6c08539aad06eeeb964ae8365ad71
Sublimez72 <vaggge@hotmail.com>
2022-10-31 13:37:10 +0100

    Fixed syntax error

--- assignments/assignment-1/verification.py
+++ assignments/assignment-1/verification.py
@@ -1,4 +1,4 @@
 import solution
 
-assert solution.solution("wrw blf hvv ozhg mrtsg'h vkrhlwv?")) == "did you see last night's episode?"
-assert solution.solution("Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!")) == "Yeah! I can't believe Lance lost his job at the colony!!"
+assert solution.solution("wrw blf hvv ozhg mrtsg'h vkrhlwv?") == "did you see last night's episode?"
+assert solution.solution("Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!") == "Yeah! I can't believe Lance lost his job at the colony!!"
