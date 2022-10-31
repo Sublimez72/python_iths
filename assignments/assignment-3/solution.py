841e6736320cf3cb38a4f7574755707553b15365
Sublimez72 <vaggge@hotmail.com>
2022-10-31 17:58:01 +0100

    finished

--- assignments/assignment-3/solution.py
+++ assignments/assignment-3/solution.py
@@ -1,52 +1,93 @@
 
 
-
-from cgitb import reset
-
-
 def solution(filename):
-    result = {
-        "Shortest word": "",
-        "Longest word": "",
-        "How many words": 0,
-        "How many lines": 1,
-        "How many spaces": 0
-        }
+    try:
+        result = {
+            "Shortest word": ["abcdefghijklmnopqrstuvwxyz"],
+            "Longest word": [""],
+            "How many words": 0,
+            "How many lines": 1,
+            "How many spaces": 0
+            }
 
+        # Counting number of occurences of each letter
+        with open(filename) as f:
+            for i in f.read():
+                
+                if i == "\n":
+                    result["How many lines"] += 1
 
-    with open(filename) as f:
-        for i in f.read():
-            
-            if i == "\n":
-                result["How many lines"] += 1
+                elif i == " ":
+                    result["How many spaces"] += 1
 
-            elif i == " ":
-                result["How many spaces"] += 1
+                elif i not in result.keys():
+                    result[i] = 1
 
-            elif i not in result.keys():
-                result[i] = 1
+                elif i in result.keys():
+                    result[i] += 1
+            # Going back to the start of the file
+            f.seek(0)
 
-            elif i in result.keys():
-                result[i] += 1
+            # Counting the number of occurences of each word and finding the longest and shortest one
+            for lines in f:
+                for word in lines.split():
+                    if word.endswith("."):
+                        word = word[:-1]
+
+                    if len(word) > len(result["Longest word"][0]):
+                        result["Longest word"] = []
+                        result["Longest word"].append(word)
+                    
+                    elif len(word) == len(result["Longest word"][0]):
+                        result["Longest word"].append(word)
+
+                    if len(word) < len(result["Shortest word"][0]):
+                        result["Shortest word"] = []
+                        result["Shortest word"].append(word)
+
+                    elif len(word) == len(result["Shortest word"][0]):
+                        result["Shortest word"].append(word)
+                    
+                    result["How many words"] += 1
+        # Sorting the list
+        result = dict(sorted(result.items()))
         
-        f.seek(0)
+        # Printing results to terminal
+        print(f"Number of lines: \t{result['How many lines']}")
+        print("-" * 50)
 
-        for lines in f:
-            for word in lines.split():
-                if word.endswith("."):
-                    word = word[:-1]
-                if len(word) > len(result["Longest word"]):
-                    result["Longest word"] = word
+        print(f"Number of words: \t{result['How many words']}")
+        print("-" * 50)
+
+        print(f"Number of spaces: \t{result['How many spaces']}")
+        print("-" * 50)
+
+        print("Longest word/words.")
+        print("-" * 50)
+        for word in result["Longest word"]:
+            print(word)
+        print()
+
+        print("Shortest word/words")
+        print("-" * 50)
+        for word in result["Shortest word"]:
+            print(word)
+        print()
 
 
+        print("Occurences per letter")
+        print("-" * 50)
+        for key in result:
+            if len(key) == 1:
+                print(f"{key}: {result[key]}")
+
+        
+        return result
     
-
-
     
-    # Sorting the list before returning it
-    result = dict(sorted(result.items()))
-    return result
+    except Exception as e:
+        print(e)
 
 
-print(solution("assignments/assignment-3/minion-submissions/minion-1.txt"))
+solution("assignments/assignment-3/minion-submissions/minion-3.txt")
 
