6a5d754499ec16771cc55deb4d01c6f45f7a1f8e
Sublimez72 <vaggge@hotmail.com>
2022-10-31 15:58:15 +0100

    commit

--- assignments/assignment-3/solution.py
+++ assignments/assignment-3/solution.py
@@ -1,5 +1,52 @@
+
+
+
+from cgitb import reset
+
+
 def solution(filename):
-    """
-    Expected return value is a dict object
-    """
-    return {}
+    result = {
+        "Shortest word": "",
+        "Longest word": "",
+        "How many words": 0,
+        "How many lines": 1,
+        "How many spaces": 0
+        }
+
+
+    with open(filename) as f:
+        for i in f.read():
+            
+            if i == "\n":
+                result["How many lines"] += 1
+
+            elif i == " ":
+                result["How many spaces"] += 1
+
+            elif i not in result.keys():
+                result[i] = 1
+
+            elif i in result.keys():
+                result[i] += 1
+        
+        f.seek(0)
+
+        for lines in f:
+            for word in lines.split():
+                if word.endswith("."):
+                    word = word[:-1]
+                if len(word) > len(result["Longest word"]):
+                    result["Longest word"] = word
+
+
+    
+
+
+    
+    # Sorting the list before returning it
+    result = dict(sorted(result.items()))
+    return result
+
+
+print(solution("assignments/assignment-3/minion-submissions/minion-1.txt"))
+
