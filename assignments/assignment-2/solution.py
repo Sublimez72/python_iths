d3f51163e646c54dffa637d67ac288730a654456
Sublimez72 <vaggge@hotmail.com>
2022-10-31 14:54:57 +0100

    Commit

--- assignments/assignment-2/solution.py
+++ assignments/assignment-2/solution.py
@@ -1,11 +1,12 @@
 
 def solution(x):
     try:
+        # Sort the list using the anonymous function lambda and list comprehension.
         x.sort(key=lambda s: [int(i) for i in s.split(".")])
     
+    # Generic exception since we don't want to print anything to the terminal
     except Exception as e:
         pass
-
 
     return x
 
