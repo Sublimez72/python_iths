b08781a70725c49f7ce6995372a7a83133abdbbc
Sublimez72 <vaggge@hotmail.com>
2022-11-02 14:05:43 +0100

    Added error catching

--- assignments/assignment-4/solution.py
+++ assignments/assignment-4/solution.py
@@ -1,19 +1,21 @@
 
 
 def solution(x, y):
+    try:
+        x_axis = 0
+        y_axis = 0
+        # calculate value on x axis with y axis 1
+        number = 0
+        for i in range(1, x + 1):
+            number = number + i
+            x_axis = number
 
-    x_axis = 0
-    y_axis = 0
-    # calculate value on x axis with y axis 1
-    number = 0
-    for i in range(1, x + 1):
-        number = number + i
-        x_axis = number
+        # calculate y axis value from variable x_axis
+        num = 0
+        for i in range(x, y + x):
+            y_axis = x_axis + num
+            num = num + i
 
-    # calculate y axis value from variable x_axis
-    num = 0
-    for i in range(x, y + x):
-        y_axis = x_axis + num
-        num = num + i
-
-    return str(y_axis)
+        return str(y_axis)
+    except Exception as e:
+        print(e)
