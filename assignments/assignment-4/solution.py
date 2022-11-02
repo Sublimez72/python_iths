9e3e97d371c4570bc8b933c4eb1b8206450d8b9c
Sublimez72 <vaggge@hotmail.com>
2022-11-02 10:35:31 +0100

    Created a solution without numpy that is faster

--- assignments/assignment-4/solution.py
+++ assignments/assignment-4/solution.py
@@ -1,45 +1,21 @@
-import numpy as np
-
-
-# Function for creating arrays.
-# Pass in the index of an array and it will calculate all the values for the next index.
-def create_array(x):
-    try:
-        num = 0
-        counter = x + 1
-
-        for i in arr[x]:
-
-            arr[x + 1, num] = i + counter
-
-            counter = counter + 1
-            num = num + 1
-    except Exception as e:
-        print(e)
 
 
 def solution(x, y):
-    try:
-        global arr
-        # I was having a problem where numpy was printing all numbers in scientific notation
-        # and this line solved it.
-        np.set_printoptions(suppress=True)
-        # square root of 100_000 rounded up
-        side = 320
-        arr = np.zeros((side, side))
 
-        # array 0
-        number = 0
-        for i in np.arange(1, side + 1):
+    arr = []
+    new_arr = []
+    # square root of 100_000 rounded up
+    side = 320
+    # array on x axis with y axis 1
+    number = 0
+    for i in range(1, side + 1):
+        number = number + i
+        arr.append(number)
+    # only calculate y axis array for the speciefied x value.
+    num = 0
+    for i in range(x, side):
+        new_arr.append(arr[x-1] + num)
+        num = num + i
 
-            number = number + i
-            arr[0, i - 1] = number
-
-        # all other arrays
-        for z in np.arange(side - 1):
-            create_array(z)
-
-        return str(int(arr[y-1, x-1]))
-
-    except Exception as e:
-        print(e)
+    result = str(new_arr[y-1])
+    return result
--- assignments/assignment-4/verification.py
+++ assignments/assignment-4/verification.py
@@ -1,20 +1,20 @@
-import solution
+import solution_with_np
 
-assert solution.solution(1, 1) == "1"
-assert solution.solution(1, 2) == "2"
-assert solution.solution(1, 3) == "4"
-assert solution.solution(1, 4) == "7"
-assert solution.solution(1, 5) == "11"
-assert solution.solution(1, 6) == "16"
-assert solution.solution(1, 7) == "22"
-assert solution.solution(1, 8) == "29"
+assert solution_with_np.solution(1, 1) == "1"
+assert solution_with_np.solution(1, 2) == "2"
+assert solution_with_np.solution(1, 3) == "4"
+assert solution_with_np.solution(1, 4) == "7"
+assert solution_with_np.solution(1, 5) == "11"
+assert solution_with_np.solution(1, 6) == "16"
+assert solution_with_np.solution(1, 7) == "22"
+assert solution_with_np.solution(1, 8) == "29"
 
-assert solution.solution(2, 1) == "3"
+assert solution_with_np.solution(2, 1) == "3"
 
-assert solution.solution(3, 1) == "6"
-assert solution.solution(3, 2) == "9"
+assert solution_with_np.solution(3, 1) == "6"
+assert solution_with_np.solution(3, 2) == "9"
 
-assert solution.solution(4, 1) == "10"
+assert solution_with_np.solution(4, 1) == "10"
 
-assert solution.solution(5, 1) == "15"
-assert solution.solution(5, 10) == "96"
+assert solution_with_np.solution(5, 1) == "15"
+assert solution_with_np.solution(5, 10) == "96"
