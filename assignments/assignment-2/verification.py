f39ed5e5ce586f44712ef3354625ee9cdd4509f4
Sublimez72 <vaggge@hotmail.com>
2022-11-03 17:52:08 +0100

    Fixed verification scripts

--- assignments/assignment-2/verification.py
+++ assignments/assignment-2/verification.py
@@ -1,9 +1,9 @@
-import solution_with_np
+import solution
 
 test_data = ["1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0"]
 expected = ["0.1", "1.1.1", "1.2", "1.2.1", "1.11", "2", "2.0", "2.0.0"]
-assert solution_with_np.solution(test_data) == expected
+assert solution.solution(test_data) == expected
 
 test_data = ["1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2"]
 expected_result = ["1.0", "1.0.2", "1.0.12", "1.1.2", "1.3.3"]
-assert solution_with_np.solution(test_data) == expected_result
+assert solution.solution(test_data) == expected_result
