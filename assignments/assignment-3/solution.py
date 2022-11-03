80698c6b636e384f63c563bb54219c4d9f37c4fa
Sublimez72 <vaggge@hotmail.com>
2022-11-03 16:43:47 +0100

    Cleanup

--- assignments/assignment-3/solution.py
+++ assignments/assignment-3/solution.py
@@ -12,23 +12,24 @@ def solution(filename):
 
         # Counting number of occurences of each letter
         with open(filename) as f:
-            for i in f.read():
-                
-                if i == "\n":
+            for letter in f.read():
+
+                if letter == "\n":
                     result["How many lines"] += 1
 
-                elif i == " ":
+                elif letter == " ":
                     result["How many spaces"] += 1
 
-                elif i not in result.keys():
-                    result[i] = 1
+                elif letter not in result.keys():
+                    result[letter] = 1
 
-                elif i in result.keys():
-                    result[i] += 1
+                elif letter in result.keys():
+                    result[letter] += 1
             # Going back to the start of the file
             f.seek(0)
 
-            # Counting the number of occurences of each word and finding the longest and shortest one
+            # Counting the number of occurences of each word
+            # and finding the longest and shortest one
             for lines in f:
                 for word in lines.split():
                     if word.endswith("."):
@@ -37,7 +38,7 @@ def solution(filename):
                     if len(word) > len(result["Longest word"][0]):
                         result["Longest word"] = []
                         result["Longest word"].append(word)
-                    
+
                     elif len(word) == len(result["Longest word"][0]):
                         result["Longest word"].append(word)
 
@@ -47,11 +48,11 @@ def solution(filename):
 
                     elif len(word) == len(result["Shortest word"][0]):
                         result["Shortest word"].append(word)
-                    
+
                     result["How many words"] += 1
         # Sorting the list
         result = dict(sorted(result.items()))
-        
+
         # Printing results to terminal
         print(f"Number of lines: \t{result['How many lines']}")
         print("-" * 50)
@@ -74,18 +75,13 @@ def solution(filename):
             print(word)
         print()
 
-
         print("Occurences per letter")
         print("-" * 50)
         for key in result:
             if len(key) == 1:
                 print(f"{key}: {result[key]}")
 
-
         return result
-    
-    
+
     except Exception as e:
         print(e)
-
-
