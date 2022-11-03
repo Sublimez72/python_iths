70e9d090f1ec7518ce8200d3548a100c0f2673b9
Sublimez72 <vaggge@hotmail.com>
2022-11-03 16:49:20 +0100

    Cleanup

--- assignments/assignment-1/solution.py
+++ assignments/assignment-1/solution.py
@@ -4,19 +4,17 @@ def solution(s):
     try:
         alphabet = "abcdefghijklmnopqrstuvwxyz"
         # Create the translation table
-        trans = str.maketrans(alphabet, alphabet[::-1] , " ")
+        trans = str.maketrans(alphabet, alphabet[::-1], " ")
         result = ""
         # Iterate through the string
         for letter in s:
-            # if the letter is lowercase we translate it using our translation table
+            # if the letter is lowercase
+            # we translate it using our translation table
             if letter.islower():
                 result += letter.translate(trans)
             else:
                 result += letter
-        
-        
         return result
 
     except Exception as e:
         print(e)
-
--- assignments/assignment-2/solution.py
+++ assignments/assignment-2/solution.py
@@ -1,7 +1,8 @@
 
 def solution(x):
     try:
-        # Sort the list using the anonymous function lambda and list comprehension.
+        # Sort the list using the anonymous function lambda
+        # using list comprehension.
         x.sort(key=lambda s: [int(i) for i in s.split(".")])
 
     except Exception as e:
