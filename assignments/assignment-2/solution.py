cbf57400c89b820673e14b31afe124d578deeae3
Sublimez72 <vaggge@hotmail.com>
2022-11-03 16:45:07 +0100

    Cleanup

--- assignments/assignment-2/solution.py
+++ assignments/assignment-2/solution.py
@@ -3,10 +3,8 @@ def solution(x):
     try:
         # Sort the list using the anonymous function lambda and list comprehension.
         x.sort(key=lambda s: [int(i) for i in s.split(".")])
-    
+
     except Exception as e:
         print(e)
 
     return x
-
-
